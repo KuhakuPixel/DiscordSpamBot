b0c0862bde177e0d65492e295541d40c5c731eee
KuhakuPixel <NicholasPixels@gmail.com>
2023-06-16 18:51:06 +0700

    add option for random forest

--- bot.py
+++ bot.py
@@ -4,34 +4,25 @@ from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import argparse
-# ==================== load model ===========
-MODEL_FILE = "nb_model.joblib"
+from model import get_model, ClassifierModelType, model_predict_is_spam
+
+# ==================== load vectorizer ===========
 VECTORIZER_FILE = "vect.joblib"
-
-nb = load(MODEL_FILE)
 vect = load(VECTORIZER_FILE)
+print(type(vect))
 
-def nb_spam_detect(nb,vect , msg: str)-> bool:
-    # 
-    x_to_predict = pd.Series(data=[msg])
-    x_to_predict_dtm = vect.transform(x_to_predict)
-    predicted = nb.predict(x_to_predict_dtm)
-    # predicted has to 1
-    assert len(predicted) == 1
 
-    # if ham = 0, spam = 1 
-    is_spam = predicted[0] == 1
+def is_bad_msg(model, msg: str) -> bool:
+    is_spam = model_predict_is_spam(model=model, vect=vect, msg=msg)
     return is_spam
 
-# ==========================================
-def is_bad_msg(msg: str)->bool:
 
-    is_spam = nb_spam_detect(nb = nb, vect = vect, msg = msg)
-    return is_spam
 # ======================================
 # string utils
-def user_id_to_mention_str(user_id: str)->str:
+def user_id_to_mention_str(user_id: str) -> str:
     return f"<@{user_id}>"
+
+
 # ======================================
 intents = discord.Intents.default()
 intents.message_content = True
@@ -41,7 +32,11 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
+
+# will be loaded at start time
+used_model = None
 
 
 @client.event
@@ -50,20 +45,31 @@ async def on_message(message):
         return
     user_id = message.author.id
     mention_user_str = user_id_to_mention_str(user_id)
-    if is_bad_msg(message.content):
-        await message.channel.send(f"{mention_user_str} Your message is deleted because it is a spam, Bing Chilllling !!!!")
+    if is_bad_msg(model=used_model, msg=message.content):
+        await message.channel.send(
+            f"{mention_user_str} Your message is deleted because it is a spam, Bing Chilllling !!!!"
+        )
         await message.delete()
     else:
-        await message.channel.send(f'{mention_user_str} Your message is good')
+        await message.channel.send(f"{mention_user_str} Your message is good")
 
 
 # ==================== CLI creation ==============
 parser = argparse.ArgumentParser()
-parser.add_argument("DISCORD_BOT_TOKEN", help="Your discord bot token" , type=str)
+parser.add_argument("DISCORD_BOT_TOKEN", help="Your discord bot token", type=str)
+parser.add_argument(
+    "--model",
+    help="Chosen Model",
+    type=ClassifierModelType,
+    choices=list(ClassifierModelType),
+    default=str(ClassifierModelType.naiveBayes),
+)
 args = parser.parse_args()
-# run the bot with the token 
+# set model to use
+print(f"model used for spam detection: {str(args.model)}") 
+used_model = get_model(modelType = args.model)
+
+# run the bot with the token
 print(args.DISCORD_BOT_TOKEN)
 client.run(args.DISCORD_BOT_TOKEN)
 # ============================================
-
-
---
+++ model.py
@@ -0,0 +1,37 @@
+"""
+    code for loading the model 
+
+"""
+from enum import Enum
+from joblib import dump, load
+import pandas as pd
+
+class ClassifierModelType(Enum):
+    naiveBayes = "naiveBayes"
+    randomForest = "randomForest"
+    def __str__(self):
+        return self.value
+
+MODEL_PATH_DICT = {
+    ClassifierModelType.naiveBayes: "nb_model.joblib",
+    ClassifierModelType.randomForest: "random_forest_model.joblib",
+}
+
+# ==================== load model ===========
+def get_model(modelType: ClassifierModelType):
+    model_file_path = MODEL_PATH_DICT[modelType]
+    return load(model_file_path)
+
+
+def model_predict_is_spam(model, vect , msg: str)-> bool:
+    # 
+    x_to_predict = pd.Series(data=[msg])
+    x_to_predict_dtm = vect.transform(x_to_predict)
+    predicted = model.predict(x_to_predict_dtm)
+    print(predicted)
+    # predicted has to 1
+    assert len(predicted) == 1
+
+    # if ham = 0, spam = 1 
+    is_spam = predicted[0] == 1
+    return is_spam
